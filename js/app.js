ca05bdd0817529ecac5cb90d38b8ec2c3f581698
Ashan Jameel <59074253+Ashan01@users.noreply.github.com>
2021-05-27 00:26:34 +0500

    commit

--- js/app.js
+++ js/app.js
@@ -2,9 +2,40 @@ let email = document.getElementById("email");
 let password = document.getElementById("password");
 
 function handleSubmit() {
-   if (email.value == "ashanjameel@gmail.com" && password.value == "pakindo") {
-      console.log("hello world");
-   } else {
-      console.log("try again");
-   }
+  alert("he;o");
+  if (email.value == "ashanjameel78@gmail.com" && password.value == "pakindo") {
+    console.log("hello world");
+  } else {
+    console.log("try again");
+  }
+}
+
+function Void(e) {
+  e.preventDefault();
+}
+
+function ChatA() {
+  var chat_Sec = document.getElementById("Chat_Sec");
+
+  var para = document.createElement("p");
+  var paraTextNode = document.createTextNode("hello");
+
+  para.appendChild(paraTextNode);
+
+  chat_Sec.appendChild(para);
+}
+
+function ChatB() {
+  var chat_Sec = document.getElementById("Chat_Sec");
+
+  var para = document.createElement("p");
+  var br = document.createElement("br");
+
+  var paraTextNode = document.createTextNode("this was good");
+  para.setAttribute("class", "paraB");
+
+  para.appendChild(paraTextNode);
+
+  chat_Sec.appendChild(para);
+  chat_Sec.appendChild(br);
 }

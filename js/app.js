3553f09b91547332c0b3c4a0e9f389a13569c75b
Ashan Jameel <59074253+Ashan01@users.noreply.github.com>
2021-05-28 01:59:04 +0500

    create ui

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+/*****************Login Js**********************/
+
 let email = document.getElementById("email");
 let password = document.getElementById("password");
 
@@ -6,36 +8,37 @@ let myForm = document.getElementById("myForm");
 myForm.addEventListener("submit", (e) => {
   e.preventDefault();
   console.log("form submitted");
-  if (email.value == "ashanjameel78@gmail.com" && password.value == "pakindo") {
-    console.log("hello world");
-    window.location.href = "../chat.html";
-  } else {
-    console.log("try again");
-  }
+  // if (email.value == "ashanjameel78@gmail.com" && password.value == "pakindo") {
+  //   console.log("hello world");
+  //   window.location.href = "http://127.0.0.1:5500/chat.html";
+  // } else {
+  //   console.log("try again");
+  //   window.location.replace("../chat.html");
+  //   // window.location.href = "../chat.html";
+  // }
 });
 
-function ChatA() {
-  var chat_Sec = document.getElementById("Chat_Sec");
+/*****************Login Js**********************/
 
-  var para = document.createElement("p");
-  var paraTextNode = document.createTextNode("hello");
+function handleMsg() {
+  let msg_input = document.getElementById("msg_input").value;
+  console.log(msg_input);
 
-  para.appendChild(paraTextNode);
+  let div1 = document.createElement("div");
+  let div2 = document.createElement("div");
+  let div3 = document.createElement("div");
+  let div4 = document.createElement("div");
+  let div5 = document.createElement("div");
+  let div6 = document.createElement("div");
+  let div7 = document.createElement("div");
 
-  chat_Sec.appendChild(para);
-}
+  div1.setAttribute("class", "msg left_msg");
+  div2.setAttribute("class", "msg_img");
+  div3.setAttribute("class", "Chat_sec");
+  div4.setAttribute("class", "msg_info");
+  div5.setAttribute("class", "msg_info_name");
+  div6.setAttribute("class", "msg_info_time");
+  div7.setAttribute("class", "msg_text");
 
-function ChatB() {
-  var chat_Sec = document.getElementById("Chat_Sec");
-
-  var para = document.createElement("p");
-  var br = document.createElement("br");
-
-  var paraTextNode = document.createTextNode("this was good");
-  para.setAttribute("class", "paraB");
-
-  para.appendChild(paraTextNode);
-
-  chat_Sec.appendChild(para);
-  chat_Sec.appendChild(br);
+  div1.appendChild(div2);
 }

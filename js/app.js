fda099613755bcbf1cf8576e50018fbdcd075d05
Ashan Jameel <59074253+Ashan01@users.noreply.github.com>
2021-06-01 01:39:24 +0500

    update

--- js/app.js
+++ js/app.js
@@ -1,10 +1,7 @@
 /****************Global Variables**************/
-let msg_input = document.getElementById("msg_input");
-let msg_input_B = document.getElementById("msg_input_B");
 let chat_Container = document.getElementById("chat_Container");
 
 let database = firebase.database().ref("chat");
-let key = database.push().key;
 
 /****************Global Variables**************/
 
@@ -32,83 +29,54 @@ myForm.addEventListener("submit", (e) => {
 /*****************Login Js**********************/
 
 function handleMsg() {
-  database.child(key).set({
-    msg_A: msg_input.value,
-  });
+  let key = database.push().key;
 
-  database.on("child_added", function (data) {
-    console.log(data.val());
-
-    let div1 = document.createElement("div");
-    let div2 = document.createElement("div");
-    let div3 = document.createElement("div");
-    let div4 = document.createElement("div");
-    let div5 = document.createElement("div");
-    let div6 = document.createElement("div");
-    let div7 = document.createElement("div");
-
-    div1.setAttribute("class", "msg right_msg");
-    div2.setAttribute("class", "msg_img_B");
-    div3.setAttribute("class", "Chat_sec Chat_sec_B");
-    div4.setAttribute("class", "msg_info");
-    div5.setAttribute("class", "msg_info_name");
-    div6.setAttribute("class", "msg_info_time");
-    div7.setAttribute("class", "msg_text");
-
-    div1.appendChild(div2);
-    div1.appendChild(div3);
-    div3.appendChild(div4);
-    div4.appendChild(div5);
-    div4.appendChild(div6);
-    div3.appendChild(div7);
-
-    var div6Text = document.createTextNode("12:56");
-    div6.append(div6Text);
-
-    var div5Text = document.createTextNode("Esha");
-    div5.append(div5Text);
-
-    var div7Text = document.createTextNode(data.val().msg_A);
-
-    div7.append(div7Text);
-    msg_input.value = "";
-
-    chat_Container.appendChild(div1);
-  });
+  // database.child(key).set({
+  //   msg_A: msg_input.value,
 }
 
 function handleMsgB() {
-  database.child(key).set({
-    msg_B: msg_input_B.value,
+  let key = database.push().key;
+
+  // database.child(key).set({
+  //   msg_B: msg_input_B.value,
+  // });
+
+  database.on("child_added", function (data) {
+    console.log(data.val());
   });
 
-  // let chat_Container = document.getElementById("chat_Container");
-  // let div1 = document.createElement("div");
-  // let div2 = document.createElement("div");
-  // let div3 = document.createElement("div");
-  // let div4 = document.createElement("div");
-  // let div5 = document.createElement("div");
-  // let div6 = document.createElement("div");
-  // let div7 = document.createElement("div");
-  // div1.setAttribute("class", "msg right_msg");
-  // div2.setAttribute("class", "msg_img");
-  // div3.setAttribute("class", "Chat_sec Chat_sec_B");
-  // div4.setAttribute("class", "msg_info");
-  // div5.setAttribute("class", "msg_info_name");
-  // div6.setAttribute("class", "msg_info_time");
-  // div7.setAttribute("class", "msg_text");
-  // div1.appendChild(div2);
-  // div1.appendChild(div3);
-  // div3.appendChild(div4);
-  // div4.appendChild(div5);
-  // div4.appendChild(div6);
-  // div3.appendChild(div7);
-  // var div6Text = document.createTextNode("12:56");
-  // div6.append(div6Text);
-  // var div5Text = document.createTextNode("Ashan");
-  // div5.append(div5Text);
-  // var div7Text = document.createTextNode(msg_input.value);
-  // div7.append(div7Text);
-  // msg_input.value = "";
-  // chat_Container.appendChild(div1);
+  let msg_input_B = document.getElementById("msg_input_B");
+
+  let chat_Container = document.getElementById("chat_Container");
+
+  let div1 = document.createElement("div");
+  chat_Container.appendChild(div1);
+
+  let div2 = document.createElement("div");
+  let div3 = document.createElement("div");
+  let div4 = document.createElement("div");
+  let div5 = document.createElement("div");
+  let div6 = document.createElement("div");
+  let div7 = document.createElement("div");
+  div1.setAttribute("class", "msg right_msg");
+  div2.setAttribute("class", "msg_img");
+  div3.setAttribute("class", "Chat_sec Chat_sec_B");
+  div4.setAttribute("class", "msg_info");
+  div5.setAttribute("class", "msg_info_name");
+  div6.setAttribute("class", "msg_info_time");
+  div7.setAttribute("class", "msg_text");
+  div1.appendChild(div2);
+  div1.appendChild(div3);
+  div3.appendChild(div4);
+  div4.appendChild(div5);
+  div4.appendChild(div6);
+  div3.appendChild(div7);
+  var div6Text = document.createTextNode("12:56");
+  div6.append(div6Text);
+  var div5Text = document.createTextNode("Ashan");
+  div5.append(div5Text);
+  var div7Text = document.createTextNode(msg_input_B.value);
+  div7.append(div7Text);
+  msg_input_B.value = "";
 }

b98003ba3162285ccc311509dc0c38f6d2cb6181
Ashan Jameel <59074253+Ashan01@users.noreply.github.com>
2021-06-01 05:29:34 +0500

    new js structure

--- js/ChatA.js
+++ js/ChatA.js
@@ -5,10 +5,6 @@ let database = firebase.database().ref("chat");
 
 /****************Global Variables**************/
 
-/*****************Login Js**********************/
-
-/*****************Login Js**********************/
-
 function handleMsg() {
   let msg_input = document.getElementById("msg_input");
 
@@ -20,77 +16,6 @@ function handleMsg() {
     time: "11:20",
   });
   msg_input.value = "";
-
-  // let chat_Container = document.getElementById("chat_Container");
-  // let div1 = document.createElement("div");
-  // chat_Container.appendChild(div1);
-  // let div2 = document.createElement("div");
-  // let div3 = document.createElement("div");
-  // let div4 = document.createElement("div");
-  // let div5 = document.createElement("div");
-  // let div6 = document.createElement("div");
-  // let div7 = document.createElement("div");
-  // div1.setAttribute("class", "msg left_msg");
-  // div2.setAttribute("class", "msg_img");
-  // div3.setAttribute("class", "Chat_sec ");
-  // div4.setAttribute("class", "msg_info");
-  // div5.setAttribute("class", "msg_info_name");
-  // div6.setAttribute("class", "msg_info_time");
-  // div7.setAttribute("class", "msg_text");
-  // div1.appendChild(div2);
-  // div1.appendChild(div3);
-  // div3.appendChild(div4);
-  // div4.appendChild(div5);
-  // div4.appendChild(div6);
-  // div3.appendChild(div7);
-  // var div6Text = document.createTextNode("12:56");
-  // div6.append(div6Text);
-  // var div5Text = document.createTextNode("Ashan");
-  // div5.append(div5Text);
-  // var div7Text = document.createTextNode(msg_input.value);
-  // div7.append(div7Text);
-}
-
-function handleMsgB() {
-  let msg_input_B = document.getElementById("msg_input_B");
-
-  let key = database.push().key;
-
-  database.child(key).set({
-    msg_B: msg_input_B.value,
-    name: "Ashan",
-    time: "11:20",
-  });
-  msg_input_B.value = "";
-
-  // let chat_Container = document.getElementById("chat_Container");
-  // let div1 = document.createElement("div");
-  // chat_Container.appendChild(div1);
-  // let div2 = document.createElement("div");
-  // let div3 = document.createElement("div");
-  // let div4 = document.createElement("div");
-  // let div5 = document.createElement("div");
-  // let div6 = document.createElement("div");
-  // let div7 = document.createElement("div");
-  // div1.setAttribute("class", "msg right_msg");
-  // div2.setAttribute("class", "msg_img");
-  // div3.setAttribute("class", "Chat_sec Chat_sec_B");
-  // div4.setAttribute("class", "msg_info");
-  // div5.setAttribute("class", "msg_info_name");
-  // div6.setAttribute("class", "msg_info_time");
-  // div7.setAttribute("class", "msg_text");
-  // div1.appendChild(div2);
-  // div1.appendChild(div3);
-  // div3.appendChild(div4);
-  // div4.appendChild(div5);
-  // div4.appendChild(div6);
-  // div3.appendChild(div7);
-  // var div6Text = document.createTextNode("12:56");
-  // div6.append(div6Text);
-  // var div5Text = document.createTextNode("Ashan");
-  // div5.append(div5Text);
-  // var div7Text = document.createTextNode(msg_input_B.value);
-  // div7.append(div7Text);
 }
 
 database.on("child_added", function (data) {
